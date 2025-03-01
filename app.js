5f623d8198ce97e1b10a00554e8d8e1c246bfe4a
altharest <altharest@gmail.com>
2025-03-01 06:59:55 -0300

    Primeiro teste

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const test = 2;
+const express = require("express");
+const axios = require("axios");
+const app = express();
 
-console.log(test);
+app.get("/cep/:cep", async (req, res) => {
+  const cep = req.params.cep;
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+  try {
+    const response = await axios.get(url);
+    const endereco = response.data;
+    console.log(endereco);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Erro ao buscar endereço" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000");
+});
