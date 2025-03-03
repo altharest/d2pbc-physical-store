51d8f746054c22fc38f5008c5a221a53a53190de
altharest <altharest@gmail.com>
2025-03-02 23:22:17 -0300

    Segundo teste

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require("express");
+const readline = require("readline");
 const axios = require("axios");
 const app = express();
 
-app.get("/cep/:cep", async (req, res) => {
-  const cep = req.params.cep;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let cep;
+
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000");
+  rl.question("Digite o CEP: ", (resposta) => {
+    cep = resposta;
+    console.log(`Pesquisando CEP ${cep}`);
+    rl.close();
+  });
+});
+
+app.get("/", async (req, res) => {
   const url = `https://viacep.com.br/ws/${cep}/json/`;
   try {
+    res.send(
+      `<body style="background-color: black"><p style="color: white">Consultando CEP: ${cep}</p></body>`
+    );
     const response = await axios.get(url);
     const endereco = response.data;
+    console.log(cep);
     console.log(endereco);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: "Erro ao buscar endereço" });
   }
-});
-
-app.listen(3000, () => {
-  console.log("Servidor iniciado na porta 3000");
 });
